18966eeb1db78ddc44c48ba7d14490ac3c0fe23e
ssskming <ssskming@126.com>
2019-02-14 21:40:16 +0800

    decortor

---
+++ src/decorator/RedShapeDecorator.java
@@ -0,0 +1,20 @@
+package decorator;
+
+public class RedShapeDecorator extends ShapeDecorator {
+
+	public RedShapeDecorator(Shape decoratedShape) {
+		super(decoratedShape);
+	}
+
+	@Override
+	public void draw() {
+		super.draw();
+		setRedBorder(decoratedShape);
+	}
+	
+	private void setRedBorder(Shape DecoratedShape){
+		System.out.println("Border Color: Red");
+	}
+	
+	
+}
---
+++ src/decorator/ShapeDecorator.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public abstract class ShapeDecorator {
+	protected Shape decoratedShape;
+	
+	public ShapeDecorator (Shape decoratedShape){
+		this.decoratedShape = decoratedShape;
+	}
+	
+	public void draw(){
+		decoratedShape.draw();
+	}
+}
